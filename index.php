b30a1fb1a76a0c1d1cb67085b2089726942f90e0
Potoisnumber1 <6511130047@mut.ac.th>
2024-07-05 17:06:28 +0700

    commit3

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <?php echo "<h1>my repository</h1>" 
-    echo "<h1>my name is tae</h1>" ?>  
+    echo "<h1>my name is tae</h1>"
+    echo "<h1>hello</h1>" ?>  
--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 <?php
-    echo"<h1>Main</h1>";   
-    echo"<h1>Porduct</h1>";
-    echo"<h1>Food</h1>";
-    echo"<h1>LOL</h1>"; 
+    echo"<h1>หน้าหลัก</h1>";   
+    echo"<h1>สินค้า</h1>";
+    echo"<h1>เกียวกับเรา</h1>";
+    echo"<h1>แผนที่</h1>"; 
 ?>
